dc814dbd0cc794debac942ad9658d28a7bbfb198
sabinonweb <sabinonweb7@gmail.com>
2026-05-12 02:26:51 +0545

    [Update: Chapter 3]

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
+use env_logger::Env;
 use ghoda::{configurations::get_configurations, startup::run};
 use sqlx::{Connection, PgPool};
 use std::net::TcpListener;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::Builder::from_env(Env::default().default_filter_or("info")).init();
+
     let configurations = get_configurations().expect("Failed to read configurations!");
     let connection = PgPool::connect(&configurations.database.connection_string())
         .await
         .expect("Failed a connection to postgres");
     let address = format!("127.0.0.1:{}", configurations.application_port);
-    let listener = TcpListener::bind(address).unwrap();
+    let listener = TcpListener::bind(address.clone()).unwrap();
+    println!("Listening on: {:?}", address);
     run(listener, connection)?.await
 }
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -11,6 +11,7 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, connection: web::Data<PgPool>) -> HttpResponse {
+    log::info!("Saving new subscriber's information in the database");
     match sqlx::query!(
         r#"
         INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -24,9 +25,12 @@ pub async fn subscribe(form: web::Form<FormData>, connection: web::Data<PgPool>)
     .execute(connection.get_ref())
     .await
     {
-        Ok(_) => HttpResponse::Ok().finish(),
+        Ok(_) => {
+            log::info!("New subscriber's information have been saved!");
+            HttpResponse::Ok().finish()
+        }
         Err(e) => {
-            println!("Failed to execute query: {}", e);
+            log::error!("Failed to execute query: {:?}", e);
             HttpResponse::InternalServerError().finish()
         }
     }
